5e1052924ae845a5faa876d839c38043c4e18e5c
Rahul Lalwani <rahul.lalwani@marketcast.com>
2023-03-05 10:30:34 +0530

    add two_dimensional_array_operations.py

---
+++ Arrays/two_dimensional_array_operations.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+# create two dimensional array
+two_d_array = np.array(
+    [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ]
+)
+print(two_d_array)
+print("------------------------")
+
+# insert row or column in a 2d array
+two_d_array = np.array(
+    [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ]
+)
+
+# adds new column at the 0th position
+new_two_d_array = np.insert(two_d_array, 0, [[100, 110, 120]], axis=1)
+print(new_two_d_array)
+
+# adds new row at the 1st position
+new_two_d_array = np.insert(two_d_array, 1, [[100, 110, 120]], axis=0)
+print(new_two_d_array)
+print("------------------------")
+
+# append row or column in a 2d array
+two_d_array = np.array(
+    [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ]
+)
+
+# append new column
+new_two_d_array = np.append(two_d_array, [[100], [110], [120]], axis=1)
+print(new_two_d_array)
+
+# append new row
+new_two_d_array = np.append(two_d_array, [[100, 110, 120]], axis=0)
+print(new_two_d_array)
+print("------------------------")
+
+# access an element of 2d array
+two_d_array = np.array(
+    [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ]
+)
+
+# access it using <array_name>[row_index][col_index]
+print(two_d_array[1, 1])
+print(two_d_array[2, 1])
+print(two_d_array[0, 1])
+print("------------------------")
+
+# traverse 2d array
+two_d_array = np.array(
+    [
+        [10, 20, 30],
+        [40, 50, 60],
+        [70, 80, 90]
+    ]
+)
+
+for row_idx in range(len(two_d_array)):
+    for col_idx in range(len(two_d_array[0])):
+        print(two_d_array[row_idx][col_idx], end="|")
+    print()
+print("------------------------")
