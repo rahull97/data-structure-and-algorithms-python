99881a3f83f2e0f7a94baf131b0f98e458f9f29e
Rahul Lalwani <rahul.lalwani@marketcast.com>
2023-03-07 12:13:50 +0530

    create list_operations.py

---
+++ Lists/list_operations.py
@@ -0,0 +1,18 @@
+# create list
+li = [10, 20, 30, 'Hello']
+print(li)
+print("----------------------")
+
+# access elements of list
+li = [10, 20, 30, 'Hello']
+print(li[1])
+print(li[-1])
+print("----------------------")
+
+# traverse through list
+li = [10, 20, 30, 'Hello']
+
+for el in li:
+    print(el)
+
+print("----------------------")
